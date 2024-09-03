5794a77b3ac041937fed0a70e1e08613e30f5dd6
MongRaGong <33287431+a06729@users.noreply.github.com>
2024-09-03 17:54:13 +0900

    클라우드 커밋 테스트

--- app/App.tsx
+++ app/App.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
   const session=await getServerSession(options);
   return (
     <main className="h-screen overflow-hidden">
-      <p>main 페이지</p>
+      <p>main 페이지2</p>
       {session?(
         <Link href="/api/auth/signout?callbackUrl=/">로그아웃</Link>
       ):(<Link href="/api/auth/signin">로그인</Link>)}
